527f2c81e73560185c4e39f7364d43163a1fb3e9
Nish Joseph <nish.joseph@leapaust.com.au>
2020-04-25 02:56:37 -0500

    Initial commit

---
+++ all/conanfile.py
@@ -0,0 +1,44 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class CgallstoolsConan(ConanFile):
+    name = "cgal-lstools"
+    version = "git"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Cgallstools here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+    _cmake = None
+
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def source(self):
+        tools.get('https://github.com/CGAL/LAStools/archive/master.zip')
+        os.rename('LAStools-master', self._source_subfolder)
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+
+    def package_info(self):
+        self.cpp_info.libs = ["cgal-lstools"]
+
